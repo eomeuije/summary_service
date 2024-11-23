3f84348c3943a8fd135d7a7c9a1b5e34b00746e0
eomeuije <euije6795@gmail.com>
2024-11-23 20:09:42 +0900

    파일 업로드 디렉토리 변경

--- controller/STT/speachMulter.js
+++ controller/STT/speachMulter.js
@@ -2,15 +2,22 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-  
-const uploadDirectory = path.join(__dirname, '..', "uploads", 'speach');
-if (!fs.existsSync(uploadDirectory)) {
-  fs.mkdirSync(uploadDirectory);
-}
+
+const getDateString = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  const uploadDirectory = path.join(__dirname, '..', '..', "uploads", 'speach', `${year}-${month}-${day}`);
+  if (!fs.existsSync(uploadDirectory)) {
+    fs.mkdirSync(uploadDirectory, { recursive: true });
+  }
+  return uploadDirectory;
+};
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDirectory);
+    cb(null, getDateString());
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
